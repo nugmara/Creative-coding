7b05b2321ed3e1981e24d51b6e7645411eb2ffdd
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-05 23:45:25 +0200

    Add another canvas element to draw on it

--- script.js
+++ script.js
@@ -25,6 +25,11 @@ window.addEventListener("load", function () {
   ctx.shadowOffSetY = 10;
   ctx.shadowOffSetX = 5;
   ctx.shadowBlur = 10;
+
+  const canvas2 = document.getElementById("canvas2");
+  const ctx2 = canvas2.getContext("2d");
+  canvas2.width = window.innerWidth;
+  canvas2.height = window.innerHeight;
   
 
   // Render a complex mathematical shape for us: The, we will take that shape and use
@@ -136,11 +141,12 @@ window.addEventListener("load", function () {
       })
     }
   }
-  const rainEffect = new Rain(canvas.width, canvas.height)
+  const rainEffect = new Rain(canvas2.width, canvas2.height)
   console.log(rainEffect)
 
   function animate() {
-    rainEffect.run();
+    ctx2.clearRect(0, 0, canvas2.width, canvas2.height)
+    rainEffect.run(ctx2);
     requestAnimationFrame(animate);
   }
   animate()
