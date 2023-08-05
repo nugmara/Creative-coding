e0ffa524a933ddee91ce5d809f4fa3313bd1e34f
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-05 19:39:07 +0200

    Add class for out project AND some comments about what we are going to build and what each class will do

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+// FRACTALS: are infinitely complex patterns that are self-similar 
+// across different scales. They are created by repeating a simple 
+// process over and over in an ongoing feedback loop
+
+// PROCEDURAL PROGRAMMING: declaring variables and funcitiones line by line
+// as we create our projects. It is the most basic way to write code.
+
 window.addEventListener("load", function(){
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext("2d");
     canvas.width = 600;
     canvas.height = 600;
+
+    // Render a complex mathematical shape for us: The, we will take that shape and use
+    // JS to convert it into pixels.
+    class Fractal {
+
+    }
+
+    // We will use Particle class to assign these pixels as a particle shape. Here, we will
+    // also deal with motion. We can do floating, raining or spinning particles if we want to.
+    // This class will contain a blueprint to define properties and behaviors of individual particles.
+    // 
+    class Particle {
+
+    }
+
+    // This class will handle the entire effect. It will define things like number of particles
+    // and so on.
+    class Rain {
+
+    }
 });
