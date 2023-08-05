a38af3756cc399c673d7d7e603b8fe09bcd69e68
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-05 20:33:11 +0200

    drawing basic shapes, in our case a line on oour Fractal class

--- script.js
+++ script.js
@@ -5,6 +5,10 @@
 // PROCEDURAL PROGRAMMING: declaring variables and funcitiones line by line
 // as we create our projects. It is the most basic way to write code.
 
+// CONSTRUCTOR: contains a blueprint and when we call the class using the "new" keyword, 
+// constructor will create one new blank JS object and it will assign it values and properties 
+// based on the blueprint inside
+
 window.addEventListener("load", function(){
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext("2d");
@@ -13,9 +17,22 @@ window.addEventListener("load", function(){
 
     // Render a complex mathematical shape for us: The, we will take that shape and use
     // JS to convert it into pixels.
-    class Fractal {
 
+    class Fractal {
+        constructor(canvasWidth, canvasHeight) {
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
+        }
+        draw(context) {
+            context.beginPath();
+            context.moveTo(0, 0);
+            context.lineTo(canvas.width, canvas.height);
+            context.stroke();
+        }
     }
+
+    const fractal1 = new Fractal(canvas.width, canvas.height);
+    fractal1.draw(ctx);
 
     // We will use Particle class to assign these pixels as a particle shape. Here, we will
     // also deal with motion. We can do floating, raining or spinning particles if we want to.
