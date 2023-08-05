85333862989b85f2923d09f8c96b4ae4a2ca17b8
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-05 23:37:53 +0200

    Create an animation on canvas

--- script.js
+++ script.js
@@ -96,9 +96,52 @@ window.addEventListener("load", function () {
   // also deal with motion. We can do floating, raining or spinning particles if we want to.
   // This class will contain a blueprint to define properties and behaviors of individual particles.
   //
-  class Particle {}
+  class Particle {
+    constructor(canvasWidth, canvasHeight) {
+      this.canvasWidth = canvasWidth;
+      this.canvasHeight = canvasHeight;
+      this.x = Math.random() * this.canvasWidth;
+      this.y = Math.random() * this.canvasHeight;
+      this.width = 50;
+      this.height = 50;
+    }
+    update() {
+      this.x++;
+      this.y++;
+    }
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
 
   // This class will handle the entire effect. It will define things like number of particles
   // and so on.
-  class Rain {}
+  class Rain {
+    constructor(canvasWidth, canvasHeight) {
+      this.canvasWidth = canvasWidth;
+      this.canvasHeight = canvasHeight;
+      this.numberOfParticles = 20;
+      this.particles = [];
+      this.#initiaize()
+    }
+    #initiaize() {
+      for ( let i = 0; i < this.numberOfParticles; i++) {
+        this.particles.push(new Particle(this.canvasWidth, this.canvasHeight));
+      }
+    }
+    run(context) {
+      this.particles.forEach((particle) => {
+        particle.draw(context)
+        particle.update()
+      })
+    }
+  }
+  const rainEffect = new Rain(canvas.width, canvas.height)
+  console.log(rainEffect)
+
+  function animate() {
+    rainEffect.run();
+    requestAnimationFrame(animate);
+  }
+  animate()
 });
